52e4896c8b9a3716447e7844611185674adc7b42
GhostMonkeyFPS <33627154+GhostMonkeyFPS@users.noreply.github.com>
2018-09-28 11:43:44 +0200

    Level class edit

--- KBS1/Level.cs
+++ KBS1/Level.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace KBS1
 {
     class Level
     {
+        private string Name { get; set; }
+
+        public Difficulty Difficulty { get; set; }
+        public Collider LevelCollider { get; set; }
+        public SpriteRenderer Renderer { get; set; }
+        public List<GameObject> Objects { get; set; }
+
+        public Level(XmlDocument xmlDocument)
+        {
+
+        }
     }
 }
