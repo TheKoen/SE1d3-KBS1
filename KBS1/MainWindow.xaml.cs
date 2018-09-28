203535f7648e6982ff59ad6ec8444cceb00e4937
Koen <koenwillemse1@gmail.com>
2018-09-28 09:51:43 +0200

    Initial commit

---
+++ KBS1/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KBS1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
