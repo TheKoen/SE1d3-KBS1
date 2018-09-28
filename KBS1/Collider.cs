eeac5fc7c6e6b9bb23f314bc04d4699c09c3acc5
Koen <koenwillemse1@gmail.com>
2018-09-28 12:08:29 +0200

    Added Colliders and Vectors

--- KBS1/Collider.cs
+++ KBS1/Collider.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KBS1
+﻿namespace KBS1
 {
     class Collider
     {
-        public Collider()
-        {
+        private readonly int radius;
+        private readonly ILocatable locatable;
 
+        public Collider(int radius, ILocatable locatable)
+        {
+            this.radius = radius;
+            this.locatable = locatable;
+        }
+
+        /// <summary>
+        /// Checks whether this Collider collides with another Collider
+        /// </summary>
+        /// <param name="collider">Collider to check for</param>
+        /// <returns>true if the Colliders collide</returns>
+        public bool Collides(Collider collider)
+        {
+            var current = locatable.Location;
+            var other = collider.locatable.Location;
+
+            return current.Distance(other) < radius + collider.radius;
         }
     }
 }
