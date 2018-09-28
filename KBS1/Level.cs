d431c2f6b257e73421ecd6169fb44f2f73eef93f
GhostMonkeyFPS <33627154+GhostMonkeyFPS@users.noreply.github.com>
2018-09-28 12:04:00 +0200

    summary Level

--- KBS1/Level.cs
+++ KBS1/Level.cs
@@ -11,11 +11,16 @@ namespace KBS1
     {
         private string Name { get; set; }
 
+        
         public Difficulty Difficulty { get; set; }
         public Collider LevelCollider { get; set; }
         public SpriteRenderer Renderer { get; set; }
         public List<GameObject> Objects { get; set; }
 
+        /// <summary>
+        /// xmlDocument loading the gamelevel field difficulty, objects, renderer and name. 
+        /// </summary>
+        /// <param name="xmlDocument">xml document for loading gamelevel</param>
         public Level(XmlDocument xmlDocument)
         {
 
