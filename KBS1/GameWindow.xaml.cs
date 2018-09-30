0ca0ab0142d16ee8baf4b8b4af46df5fdc02b09c
Dennis Kok <sslacyhadals@gmail.com>
2018-09-30 20:08:46 +0200

    Cleaned up GameWindow, added ObjectInfoContainer, fixed .gitignore

--- KBS1/GameWindow.xaml.cs
+++ KBS1/GameWindow.xaml.cs
@@ -5,35 +5,32 @@ using System.Windows.Shapes;
 
 namespace KBS1 {
     public partial class GameWindow : Window {
-        public GameWindow()
-        {
-            InitializeComponent();
-            Ellipse cirkel = new Ellipse();
-            cirkel.Fill = Brushes.ForestGreen;
-            cirkel.Width = 150;
-            cirkel.Height = 150;
-            Canvas.SetLeft(cirkel, 50);
-            Canvas.SetTop(cirkel, 70);
-            DrawingPanel.Children.Add(cirkel);
-        }
 
-        
-        /// <summary>
-        /// Properties
-        /// </summary>
+        // Properties
         public Canvas DrawPanel { get; set; }
         public Level Loadedlevel { get; set; }
         //public SoundManager SManager { get; set; }
         //public GameLoop Loop { get; set; }
 
-        /// <summary>
-        /// Methods
-        /// </summary>
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            Ellipse circle = new Ellipse();
+            circle.Fill = Brushes.ForestGreen;
+            circle.Width = 150;
+            circle.Height = 150;
+            Canvas.SetLeft(circle, 50);
+            Canvas.SetTop(circle, 70);
+            DrawingPanel.Children.Add(circle);
+        }
+        
+
+        // Methods
         public GameWindow Current()
         {
             return null;
         }
                 
-    }
     }
 }
