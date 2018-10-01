08be7ef8bbb3db9dc821476908c2db027e7e52dd
Dennis Kok <sslacyhadals@gmail.com>
2018-10-01 11:52:18 +0200

    Created example level XML, implemented basic level parsing

--- KBS1/Level.cs
+++ KBS1/Level.cs
@@ -18,7 +18,37 @@ namespace KBS1
         /// <param name="xmlDocument">xml document for loading gamelevel</param>
         public Level(XmlDocument xmlDocument)
         {
+            var root = xmlDocument.DocumentElement;
 
+            if (!root.HasAttribute("name")) throw new XmlException("Level missing name attribute");
+            Name = root.GetAttribute("name");
+            
+            var objectsXml = xmlDocument.SelectSingleNode("//level/objects");
+            if (objectsXml == null) throw new XmlException("Level missing objects node");
+            // Parsing objects
+            foreach (var child in objectsXml.ChildNodes)
+            {
+                if (!(child is XmlNode)) continue;
+                XmlNode childXml = (XmlNode)child;
+                if (childXml.LocalName == "start") CreateStartPoint(childXml);
+                if (childXml.LocalName == "end") CreateEndPoint(childXml);
+                if (childXml.LocalName == "obstacle") CreateObstacle(childXml);
+            }
+        }
+
+        private void CreateStartPoint(XmlNode node)
+        {
+            // TODO: Create StartPoint object
+        }
+
+        private void CreateEndPoint(XmlNode node)
+        {
+            // TODO: Create EndPoint object
+        }
+
+        private void CreateObstacle(XmlNode node)
+        {
+            // TODO: Create Obstacle object
         }
     }
 }
