3ae5c1cddc7d0d9a2328a738b266846be30bfbfc
GhostMonkeyFPS <33627154+GhostMonkeyFPS@users.noreply.github.com>
2018-10-02 11:09:36 +0200

    bruh

---
+++ KBS1/CreeperObstacleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBS1
+{
+    public class CreeperObstacleController : ObstacleController
+    {
+        private int speed = 1;
+
+        public CreeperObstacleController(ILocatable locatable, Obstacle obstacle) : base(locatable, obstacle) { }
+
+        public override void Update()
+        {
+            // player object
+            var playerObject = FindPlayer();
+
+            // player location
+            var player = playerObject.Location;
+
+            var xDistance = player.AxisDistance(Object.Location, true);
+            var yDistance = player.AxisDistance(Object.Location, false);
+            
+            
+
+            if (xDistance > yDistance)
+            {
+                if(xDistance < 0)
+                {
+                    Move(new Vector(speed, 0));
+                }
+                else
+                {
+                    Move(new Vector(-speed, 0));
+                }
+            }
+            if(xDistance < yDistance)
+            {
+                if (yDistance < 0)
+                {
+                    Move(new Vector(0, speed));
+                }
+                else
+                {
+                    Move(new Vector(0, -speed));
+                }  
+            }
+        }
+    }
+}
