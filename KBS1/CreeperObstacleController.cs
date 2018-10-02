f232b8bcf55f2e7f1e0f5537dbf69d5a5245b6cc
GhostMonkeyFPS <33627154+GhostMonkeyFPS@users.noreply.github.com>
2018-10-02 12:26:41 +0200

    Noice creeper werkt een beetje

--- KBS1/CreeperObstacleController.cs
+++ KBS1/CreeperObstacleController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace KBS1
@@ -9,7 +10,10 @@ namespace KBS1
     public class CreeperObstacleController : ObstacleController
     {
         private int speed = 1;
-
+        private int range = 300;
+        private int explosionRadius = 100;
+        private int delayCreeper = 100;
+        private int wait = 0;
         public CreeperObstacleController(ILocatable locatable, Obstacle obstacle) : base(locatable, obstacle) { }
 
         public override void Update()
@@ -20,33 +24,57 @@ namespace KBS1
             // player location
             var player = playerObject.Location;
 
-            var xDistance = player.AxisDistance(Object.Location, true);
-            var yDistance = player.AxisDistance(Object.Location, false);
-            
+            if (wait > 0)
+            {
+                wait--;
+                if (wait == 0 && Object.Location.Distance(player) < explosionRadius)
+                {
+                    GameWindow.Current().Reset();
+                }
+                return;
+            } 
             
 
+            var xDistance = player.AxisDistance(Object.Location, true);
+            var yDistance = player.AxisDistance(Object.Location, false);
+
+            if (player.Distance(Object.Location) > range)
+            {
+                return;
+            }
+
+            var result = false;
             if (xDistance > yDistance)
             {
-                if(xDistance < 0)
+                if(player.X > Object.Location.X)
                 {
-                    Move(new Vector(speed, 0));
+                    result = Move(new Vector(speed, 0));
                 }
                 else
                 {
-                    Move(new Vector(-speed, 0));
+                    result = Move(new Vector(-speed, 0));
                 }
             }
-            if(xDistance < yDistance)
-            {
-                if (yDistance < 0)
+            else
+            { 
+                if (player.Y > Object.Location.Y)
                 {
-                    Move(new Vector(0, speed));
+                    result = Move(new Vector(0, speed));
                 }
                 else
                 {
-                    Move(new Vector(0, -speed));
+                    result = Move(new Vector(0, -speed));
                 }  
             }
+            // result if not moving/collided
+            if(result == false)
+            {
+                wait = delayCreeper;
+                
+
+            }
+            
+
         }
     }
 }
--- KBS1/ObstacleType.cs
+++ KBS1/ObstacleType.cs
@@ -41,10 +41,13 @@ namespace KBS1
             }
             throw new NullReferenceException($"ObstacleType {name} could not be found");
         }
-
+        //create obstacle
         public static void Init()
         {
+            // runner
             Types.Add(new ObstacleType(new RunnerObstacle(), 24, Level.LoadImage("runner.png")));
+            // creeper
+            Types.Add(new ObstacleType(new CreeperObstacle(), 24, Level.LoadImage("creeper.png")));
         }
 
         private class RunnerObstacle : IControllerCreator
@@ -52,6 +55,13 @@ namespace KBS1
             public Controller Create(Obstacle obstacle)
             {
                 return new RunnerObstacleController(obstacle, obstacle);
+            }
+        }
+        private class CreeperObstacle : IControllerCreator
+        {
+            public Controller Create(Obstacle obstacle)
+            {
+                return new CreeperObstacleController(obstacle, obstacle);
             }
         }
     }
