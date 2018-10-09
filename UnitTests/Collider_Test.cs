0ec39fc77f91e8165e6ac542fc3efc7791d2bb8e
s1118049 <38284664+s1118049@users.noreply.github.com>
2018-10-09 13:15:56 +0200

    unittest + player spawn

---
+++ UnitTests/Collider_Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using KBS1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Collider_Test
+    {
+        [Test]
+        public void TestMethod1(bool result)
+        {
+            //arrange
+            Collider collider1 = new Collider(24, );
+            Collider collider2 = new Collider(24, );
+            bool? answer1 = null;
+            bool? answer2 = null;
+
+            //act
+            answer1 = collider1.Collides();
+
+            //assert
+            NUnit.Framework.Assert.AreEqual(answer1, result);
+
+        }
+    }
+}
